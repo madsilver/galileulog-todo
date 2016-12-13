a4964d86ac079ecd41f3f65d0d236e699603ca3f
Silver <rodrigo-prata@hotmail.com>
2016-12-13 11:30:55 -0200

    Criando TodoController

---
+++ src/AppBundle/Controller/TodoController.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: silver
+ * Date: 12/13/16
+ * Time: 11:04 AM
+ */
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends Controller
+{
+    /**
+     * @Route("/todos", name="todo_list")
+     */
+    public function listAction() {
+        return $this->render('todo/index.html.twig');
+    }
+
+    /**
+     * @Route("/todo/create", name="todo_create")
+     */
+    public function createAction(Request $request) {
+        return $this->render('todo/create.html.twig');
+    }
+
+    /**
+     * @Route("/todo/edit/{id}", name="todo_edit")
+     */
+    public function editAction($id, Request $request) {
+        return $this->render('todo/edit.html.twig');
+    }
+
+    /**
+     * @Route("/todo/details/{id}", name="todo_details")
+     */
+    public function detailsAction($id) {
+        return $this->render('todo/details.html.twig');
+    }
+}
